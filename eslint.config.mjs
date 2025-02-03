0287db8cac48d90cce70e10ba997f65cf532a360
arimu <arimurahiroaki40@gmail.com>
2025-02-03 22:20:59 +0900

    Eslintエラーを緩和

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // 未使用変数を警告に
+      "@next/next/no-async-client-component": "warn", // asyncクライアントコンポーネントのエラーを警告に
+    },
+  },
 ];
 
 export default eslintConfig;
